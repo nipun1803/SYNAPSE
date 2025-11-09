904558bde3888651bd1791797f42e709912be5d2
Nipun Reddy <nipun2006134@gmail.com>
2025-11-09 11:13:56 +0530

    error fixed

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { me } from "../controllers/authController.js";
-import authUser from "../middleware/authuser.js";
+import authUser from "../middleware/authUser.js";
 
 const router = express.Router();
 
