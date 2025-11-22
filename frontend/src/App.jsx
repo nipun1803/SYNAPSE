283a095c88944ef103609ea018e2988a33d1d736
Nipun Reddy <nipun2006134@gmail.com>
2025-11-22 11:33:19 +0530

    fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ import MyProfile from './pages/MyProfile'
 import Footer from './components/Footer'
 import { useAuth } from './hooks/useAuth'
 import { ToastContainer } from 'react-toastify'
-import "react-toastify/dist/react-toastify.css";
+import "react-toastify/dist/ReactToastify.css";
 
 // Protected Route Wrapper
 const ProtectedRoute = ({ children }) => {
