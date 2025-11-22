d083dc18a48defa98e23c68318d011dd95243e24
Nipun Reddy <nipun2006134@gmail.com>
2025-11-22 11:12:36 +0530

    updated toast and tailwind

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: {
-        "@tailwindcss/postcss": {},
-      },
-    },
-    transformerOptions: {
-      exclude: [/node_modules/],
-    },
-  },
   server: {
     port: 5173,
     proxy: {
